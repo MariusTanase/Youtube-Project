88dc9b491aad81a9a11bf4aae0cd9f7a367d7e0c
MariusTanase <pulsemusic8@hotmail.com>
2023-12-28 20:48:45 +0000

    Adding Routes, basic folder structure, basic files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+
 
 function App() {
-  return <p>Hello World</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home Babeh</div>
+  )
+}
+
+export default Home
